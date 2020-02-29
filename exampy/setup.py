211994067a2ab3200cf6cf76d0fd5bb0cc794a51
Jo Bovy <bovy@astro.utoronto.ca>
2020-02-29 12:27:34 -0500

    Also add setup.py for exampy

---
+++ exampy/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="exampy",
+    version="0.0.1",
+    author="Jo Bovy",
+    author_email="bovy@astro.utoronto.ca",
+    description="A small example package",
+    packages=["exampy","exampy/submodule1"]
+)
