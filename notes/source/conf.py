3334effb183f4e7be6e19281f53abd55c92b87d9
Jo Bovy <bovy@astro.utoronto.ca>
2020-02-29 12:30:25 -0500

    Add copy button to code examples

--- notes/source/conf.py
+++ notes/source/conf.py
@@ -42,7 +42,7 @@ rst_epilog = """
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['nbsphinx',"sphinx.ext.extlinks"]
+extensions = ['nbsphinx',"sphinx.ext.extlinks","sphinx_copybutton"]
 
 # Link to PDF version that includs the git hash
 extlinks = {'pdf_link': ('pdf/code-packaging-rev{}.pdf'.format(git_hash),
