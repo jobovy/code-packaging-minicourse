9fc2b09d52e0b2c8ae0b5cd4e3d42e9066c9f5fb
Jo Bovy <bovy@astro.utoronto.ca>
2020-03-01 00:18:01 -0500

    Docstring for cube function, with all the bells and whistles

--- exampy/exampy/submodule1/subutils.py
+++ exampy/exampy/submodule1/subutils.py
@@ -1,2 +1,43 @@
 def cube(x):
+    """The cube of a number
+
+Calculates and returns the cube of any floating-point number;
+note that, as currently written, the function also works for
+arrays of floats, ints, arrays of ints, and more generally,
+any number or array of numbers.
+
+Parameters
+----------
+x: float
+    Number to cube
+
+Returns
+-------
+float
+    Cube of x
+
+Raises
+------
+No exceptions are raised.
+
+See Also
+--------
+exampy.square: Square of a number
+exampy.pow: a number raised to an arbitrary power
+
+Notes
+-----
+Implements the standard cube function
+
+.. math:: f(x) = x^3
+
+References
+----------
+.. [1] A. Mathematician, "x to the p-th power: squares, cubes, and their 
+   general form," J. Basic Math., vol. 2, pp. 2-3, 1864.
+
+History
+-------
+2020-03-01: First implementation - Bovy (UofT)
+"""
     return x**3
