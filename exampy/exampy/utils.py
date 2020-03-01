d194cb0dcac3ff61aaec939e0d4e7b03f05fe9a2
Jo Bovy <bovy@astro.utoronto.ca>
2020-02-29 23:51:02 -0500

    Add pow function and its docstring

--- exampy/exampy/utils.py
+++ exampy/exampy/utils.py
@@ -4,11 +4,28 @@ def square(x):
 Parameters
 ----------
 x: float
-   Number to square
+    Number to square
 
 Returns
 -------
 float
-   Square of x
+    Square of x
 """
     return x**2.
+
+def pow(x,p=2.):
+    """A number x raised to the p-th power
+
+Parameters
+----------
+x: float
+    Number to raise to the power p
+p: float, optional
+    Power to raise x to
+
+Returns
+-------
+float
+    x^p
+"""
+    return x**p
